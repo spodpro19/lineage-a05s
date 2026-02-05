41ee685e374442c8de70173da4be912ebecdab0a
spodpro19 <spodpro17@gmail.com>
2026-02-05 19:54:04 +0530

    Create download_ap.sh

---
+++ download_ap.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+# Create firmware folder
+mkdir -p firmware
+
+# Replace <FILE_ID> with your Google Drive file ID
+FILE_ID="<YOUR_FILE_ID>"
+
+# Download AP from Google Drive
+curl -c /tmp/cookies "https://drive.google.com/uc?export=download&id=${FILE_ID}" > /dev/null
+curl -L -b /tmp/cookies "https://drive.google.com/uc?export=download&confirm=$(awk '/download/ {print $NF}' /tmp/cookies)&id=${https://drive.google.com/file/d/1wg8p5Z1Gw3qzUQnq9WYnBw1iLMmcBLD8/view?usp=sharing}" -o firmware/AP_A057F.tar.md5
+
+echo "AP firmware downloaded successfully"
